28d3231187af458746b74b40f67299ee3021cb98
dilkash722 <dilkashalam026@gmail.com>
2025-04-12 13:47:58 +0530

    Added root route

--- server.js
+++ server.js
@@ -14,6 +14,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Root Route
+app.get("/", (req, res) => {
+  res.send("Running Server...");
+});
+
 // Routes
 app.use("/api/admission", admissionRoutes);
 app.use("/api/registration", registrationRoutes);
